0aa6084f3a202b0bc7187c796f6ac9b0b9d9a8c3
platinadoc <culture_6@i.ua>
2022-08-10 22:52:30 +0300

    code refactoring on react-hw-04-images

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,16 @@
 import Skeleton from 'react-loading-skeleton';
-import ImageGallery from '../ImageGallery/ImageGallery';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 const Loader = () => {
   const array = Array(12).fill(0);
   return (
-    <ImageGallery>
+    <ul classname="ImageGallery">
       {array.map((el, idx) => (
         <li className="imageGalleryItem" key={idx}>
           <Skeleton style={{ width: 353 }} className="imageGalleryItem-image" />
         </li>
       ))}
-    </ImageGallery>
+    </ul>
   );
 };
 
